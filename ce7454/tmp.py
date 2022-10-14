599b710af6ebf8f6d81988574eb64e1d6b136121
sparkjiao <jiaofangkai@hotmail.com>
2022-10-14 13:20:15 +0800

    * Add pseudo code in pytorch style.

---
+++ ce7454/tmp.py
@@ -0,0 +1,28 @@
+
+# net - CLIP model
+# pos_index[b, max_pos] - the indices of the positive relations
+# neg_index[b, max_neg] - the indices of the negative relations
+# pos_mask[b, max_pos]  - the mask indicating true values in pos_index
+# neg_mask[b, max_neg]  - the mask indicating true values in neg_index
+
+# forward
+logits_per_image, _ = net(batch["data"], batch["text"])  # [batch, rel_num]
+
+pos_index = batch["pos_index"]
+neg_index = batch["neg_index"]
+pos_mask = batch["pos_mask"]
+neg_mask = batch["neg_mask"]
+labels = batch["labels"]
+labels[~pos_mask.bool()] = -100  # Remove the loss of padding positions
+
+pos_scores = torch.gather(logits_per_image, dim=1, index=pos_index)
+neg_scores = torch.gather(logits_per_image, dim=1, index=neg_index)
+neg_scores[~neg_mask.bool()] = -10000
+# [batch_size, max_pos_num, 1]
+pos_scores = pos_scores.unsqueeze(-1)
+# [batch, max_pos_num, max_neg_num]
+neg_scores = neg_scores.unsqueeze(1).expand(-1, pos_index.size(1), -1)
+# [batch, max_pos_num, max_neg_num + 1]
+scores = torch.cat([pos_scores, neg_scores], dim=2)
+loss = CrossEntropyLoss(ignore_index=-100)(scores.reshape(-1, neg_index.size(1) + 1),
+                                           labels.reshape(-1))
